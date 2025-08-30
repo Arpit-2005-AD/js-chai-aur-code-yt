00086b2755ee3c3165e953dd744329b966b360e2
Arpit-2005-AD <arpitdas716@gmail.com>
2025-08-30 21:47:18 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";  //writing this , now js will treat this file as newer version
+
+//alert("hello") //we are using nodejs so will not run
+
+// number => 2 to the power of 53 or use BigInt
+// boolean => true/false
+//null => standalone value
+
+//null is a object in js , its a mistake but ok , asked in interview
+
+console.log(typeof null);
