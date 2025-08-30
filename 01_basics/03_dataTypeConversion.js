3df6667625da75a81e28617e454e17b42776e064
Arpit-2005-AD <arpitdas716@gmail.com>
2025-08-31 01:15:39 +0530

    conversion of datatypes

---
+++ 01_basics/03_dataTypeConversion.js
@@ -0,0 +1,11 @@
+// let score = null
+let score = "55abd"
+//let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+//Number conversion happens but its not accurate cause js have some problem
