e4a43f536dfc5684ec931ef90355d79691246ca5
Arpit-2005-AD <arpitdas716@gmail.com>
2025-08-31 19:59:18 +0530

    datatypes 2nd time

--- 01_basics/03_dataTypeConversion.js
+++ 01_basics/03_dataTypeConversion.js
@@ -9,3 +9,26 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber)
 //Number conversion happens but its not accurate cause js have some problem
+
+
+//*****************************************operations************************************* */
+
+
+// console.log(2**5);
+// //works like power
+// console.log(2%3);
+// console.log(1%6);
+
+let str1 = "Arpit"
+let str2 = " Das"
+let str3 = str1 + str2
+console.log(str1 + str2);
+console.log(str3);
+
+console.log(true);
+console.log(+true);
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
