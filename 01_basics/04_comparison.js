03d7aacae7b9ab81368a1c8557f3d2db92d44ab3
Arpit-2005-AD <arpitdas716@gmail.com>
2025-08-31 20:25:02 +0530

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+console.log(2 > 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//in comparison null is treated like 0 
+// === strictly checks as if it is same datatype and also value
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+
+console.log(id == anotherId);
+console.log(id
+    
+);
+
